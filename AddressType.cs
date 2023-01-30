ae252c1fb0b3bf1e58f8d254a0a4c142d7ebd7ec
m911 <mitaka911@gmail.com>
2023-01-30 20:49:33 +0200

    1

---
+++ AddressType.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AddressBookAPI
+{
+	public class AddressType
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		[StringLength(20)]
+		public string FirstName { get; set; } = string.Empty;
+
+		[Required]
+		[StringLength(20)]
+		public string LastName { get; set; } = string.Empty;
+
+		[Required]
+		public int TelNumber { get; set; }
+	}
+}
